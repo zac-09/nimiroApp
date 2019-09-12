8c3f3ff7891825feb1e3058e7cc9369c8f5dcae9
Daniel Ssejjemba <ssejjembadan@gmail.com>
2019-09-12 19:22:32 +0300

    Add welcome screen

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,11 +2,19 @@ import logo from './logo.png';
 import bg from './splash_bg.png';
 import bg2 from './splash_bg2.png';
 import back from './back.jpg';
+import cmp1 from './rotary_logo.png';
+import cmp2 from './rotaract_logo.png';
+import cmp3 from './interact_logo.png';
+import smallLogo from './small_logo.png';
 
 
 export {
     logo,
     bg,
     bg2,
-    back
+    back,
+    cmp1,
+    cmp2,
+    cmp3,
+    smallLogo
 }
--- src/screens/welcome/Welcome.js
+++ src/screens/welcome/Welcome.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import { View, Text } from 'native-base'
+import React from 'react';
+import styled from 'styled-components/native'
+import { smallLogo, bg2, cmp1, cmp2, cmp3} from '../../assets'
 
-export default class Welcome extends React.Component{
+class Welcome extends React.Component {
+
+    componentDidMount(){
+        setTimeout(()=> {
+            this.props.navigation.navigate('Login')
+        }, 3000)
+    }
 
     render(){
-        return(
-            <View style={{alignItems: "center", justifyContent: "center"}}>
-                <Text>Welcome</Text>
-            </View>
+        return (
+            <Container source={bg2}>
+                <LogoContainer>
+                    <Logo source={smallLogo} resizeMode="contain" />
+                    <Slogan>Service above self</Slogan>
+                </LogoContainer>
+                <CompanyLogo source={cmp1} resizeMode="contain" />
+                <CompanyLogo source={cmp2} resizeMode="contain" />
+                <CompanyLogo source={cmp3} resizeMode="contain" />
+            </Container>
         )
     }
 }
+
+export default Welcome;
+const Container = styled.ImageBackground`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+`
+const LogoContainer = styled.View`
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding-top: 20;
+    padding-bottom: 20;
+`
+
+const Logo = styled.Image`
+    width: 60%;
+    margin-bottom: 10;
+`
+
+const Slogan = styled.Text`
+    text-align: center;
+    font-size: 16;
+    font-weight: bold;
+    color: #850127;
+    text-transform: capitalize;
+    font-style: italic;
+    margin-bottom: 30;
+`
+
+const CompanyLogo = styled.Image`
+    width: 90%;
+    margin-bottom: 10;
+`
